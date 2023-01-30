4366193283266b7bb63277d99d2bb4ccb4a8a156
sunny <sunny@jbcole.co.uk>
2023-01-30 17:26:11 +0000

    Implemented basic search bar in App.js

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, StatusBar, Platform, View, Text } from 'react-native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { Searchbar } from 'react-native-paper';
 
 const App = () => {
   return (
     <>
       <SafeAreaView style={styles.safeArea}>
-        <View style={styles.container}>
-          <Text>Welcome to MealsToGo</Text>
+        <View style={styles.containerOne}>
+          <Searchbar placeholder="Search" />
+        </View>
+        <View style={styles.containerTwo}>
+          <Text>list</Text>
         </View>
       </SafeAreaView>
       <ExpoStatusBar style="auto" />
@@ -20,9 +24,14 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
-  container: {
+  containerOne: {
+    padding: 16,
+    backgroundColor: 'green',
+  },
+  containerTwo: {
     flex: 1,
     padding: 16,
+    backgroundColor: 'blue',
   },
 });
 
